5e0d005f7b056f2bb859efd01431402009aa67f0
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-05-24 15:32:18 +0100

    Chore(BE/Project): Convert To Adonis API Only

--- canopy-scheme-be/start/routes.js
+++ canopy-scheme-be/start/routes.js
@@ -6,14 +6,16 @@
 |--------------------------------------------------------------------------
 |
 | Http routes are entry points to your web application. You can create
-| routes for different URL's and bind Controller actions to them.
+| routes for different URLs and bind Controller actions to them.
 |
 | A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
+| http://adonisjs.com/docs/4.0/routing
 |
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.get('/', () => {
+  return { greeting: 'Hello world in JSON' }
+})
