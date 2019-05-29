6cc88c0c8be303900315833647b81794f95a52b4
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2019-05-29 18:17:10 +0100

    chore: remove help text of email for login

--- canopy-scheme-fe/src/components/AuthComponents/login.js
+++ canopy-scheme-fe/src/components/AuthComponents/login.js
@@ -50,9 +50,6 @@ class Login extends React.Component {
                             value={email}
                             onChange={this.handleChange}
                         />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
                     </Form.Group>
 
                     <Form.Group controlId="formBasicPassword">
