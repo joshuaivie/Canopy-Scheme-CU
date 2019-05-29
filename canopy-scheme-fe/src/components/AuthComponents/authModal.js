08b0a8af18211987cbb96e3c6a5f9faad1403cdc
John Ayeni <johnayeni10.aj@gmail.com>
2019-05-29 18:12:37 +0100

    chore: design changes to onboarding page and buttons

--- canopy-scheme-fe/src/components/AuthComponents/authModal.js
+++ canopy-scheme-fe/src/components/AuthComponents/authModal.js
@@ -1,39 +1,35 @@
-import React from 'react';
-import { Modal } from 'react-bootstrap';
-import LoginComponent from './login';
-import RegisterComponent from './register';
+import React from "react";
+import { Modal } from "react-bootstrap";
+import LoginComponent from "./login";
+import RegisterComponent from "./register";
 
 class AuthModal extends React.Component {
-  state = {
-    form: 'login',
-  };
+    state = {
+        form: "login"
+    };
 
-  switchForm = () => {
-    const { form } = this.state;
-    this.setState({ form: form === 'login' ? 'register' : 'login' });
-  };
+    switchForm = () => {
+        const { form } = this.state;
+        this.setState({ form: form === "login" ? "register" : "login" });
+    };
 
-  render() {
-    const { show, toggleModal } = this.props;
-    const { form } = this.state;
-    return (
-		<Modal size="lg" show={show} onHide={toggleModal}>
-			<Modal.Header closeButton>
-				<Modal.Title style={{ textAlign: "center", fontSize: "2rem" }}>
-					Welcome to Canopy Scheme
-				</Modal.Title>
-			</Modal.Header>
-			<Modal.Body>
-				{form === "login" && (
-					<LoginComponent switchForm={this.switchForm} />
-				)}
-				{form === "register" && (
-					<RegisterComponent switchForm={this.switchForm} />
-				)}
-			</Modal.Body>
-		</Modal>
-	);
-  }
+    render() {
+        const { show, toggleModal } = this.props;
+        const { form } = this.state;
+        return (
+            <Modal size="lg" show={show} onHide={toggleModal}>
+                <Modal.Header closeButton />
+                <Modal.Body>
+                    {form === "login" && (
+                        <LoginComponent switchForm={this.switchForm} />
+                    )}
+                    {form === "register" && (
+                        <RegisterComponent switchForm={this.switchForm} />
+                    )}
+                </Modal.Body>
+            </Modal>
+        );
+    }
 }
 
 export default AuthModal;
--- canopy-scheme-fe/src/components/AuthComponents/login.js
+++ canopy-scheme-fe/src/components/AuthComponents/login.js
@@ -3,99 +3,100 @@ import { Form, Button, Container } from "react-bootstrap";
 import { post } from "../../utils/fetch";
 
 class Login extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: "",
-			password: "",
-			isLoading: false,
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: "",
+            isLoading: false
+        };
+    }
 
-	handleChange = event => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
-	handleSubmit = event => {
-		event.preventDefault();
-		this.login();
-	};
+    handleSubmit = event => {
+        event.preventDefault();
+        this.login();
+    };
 
-	async login() {
-		try {
-			const { email, password } = this.state;
-      console.log(email, password);
-      const res = await post("login", {email, password });
-      localStorage.setItem("authToken", res.data.token);
-      let {history} = this.props;
-      window.setTimeout(function() {
-        history.push("/app");
-      }, 2000);
-		} catch (err) {
-			this.setState({ isLoading: false });
-		}
-	}
+    async login() {
+        try {
+            const { email, password } = this.state;
+            console.log(email, password);
+            const res = await post("login", { email, password });
+            localStorage.setItem("authToken", res.data.token);
+            let { history } = this.props;
+            window.setTimeout(function() {
+                history.push("/app");
+            }, 2000);
+        } catch (err) {
+            this.setState({ isLoading: false });
+        }
+    }
 
-	render() {
-		const { email, password, isLoading } = this.state;
-		const { switchForm } = this.props;
-		if (isLoading) {
-			return;
-		}
-		return (
-			<Container>
-				<Form onSubmit={this.handleSubmit}>
-					<div className="text-center">
-						<h3 className="dark-grey-text">
-							<strong>Login to continue</strong>
-						</h3>
-					</div>
-					<Form.Group controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control
-							type="email"
-							placeholder="Enter your email"
-							name="email"
-							value={email}
-							onChange={this.handleChange}
-						/>
-						<Form.Text className="text-muted">
-							We'll never share your email with anyone else.
-						</Form.Text>
-					</Form.Group>
+    render() {
+        const { email, password, isLoading } = this.state;
+        const { switchForm } = this.props;
+        if (isLoading) {
+            return;
+        }
+        return (
+            <Container>
+                <Form onSubmit={this.handleSubmit}>
+                    <div className="text-center">
+                        <h4 className="dark-grey-text">
+                            <strong>Login to your account</strong>
+                        </h4>
+                    </div>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control
+                            type="email"
+                            placeholder="Enter your email"
+                            name="email"
+                            value={email}
+                            onChange={this.handleChange}
+                        />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-					<Form.Group controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control
-							type="password"
-							placeholder="Password"
-							name="password"
-							value={password}
-							onChange={this.handleChange}
-						/>
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Login
-					</Button>
-					<p>
-						Don't have an account?{" "}
-						<span
-							onClick={() => {
-								switchForm("register");
-							}}
-							style={{
-								textDecoration: "underline",
-                fontStyle: "italic",
-                cursor: "grabbing"
-							}}
-						>
-							Register
-						</span>
-					</p>
-				</Form>
-			</Container>
-		);
-	}
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            value={password}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Login
+                    </Button>
+                    <br />
+                    <p>
+                        Don't have an account?{" "}
+                        <span
+                            onClick={() => {
+                                switchForm("register");
+                            }}
+                            style={{
+                                textDecoration: "underline",
+                                fontStyle: "italic",
+                                cursor: "grabbing"
+                            }}
+                        >
+                            Register
+                        </span>
+                    </p>
+                </Form>
+            </Container>
+        );
+    }
 }
 
 export default Login;
