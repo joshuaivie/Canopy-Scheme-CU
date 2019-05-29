0744b1cf0303a07f09fe099e8888b1687a1df6b6
👀 <faithid10@gmail.com>
2019-05-29 16:12:22 +0100

    Feature: Payment Modal

--- canopy-scheme-fe/src/components/DashboardComponents/profile.js
+++ canopy-scheme-fe/src/components/DashboardComponents/profile.js
@@ -1,15 +1,26 @@
-import React from 'react';
-
+import React from "react";
+import { Card, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class Profile extends React.Component {
     state = {
         profile: []
-    }
+    };
     render() {
         return (
-            <div>
-                <p>This is the profile</p>
-            </div>
-        )
+            <Col xs="12" md="12">
+                <Card className="material-card">
+                    <Card.Body>
+                        <h5>Profile</h5>
+                        <div className="text-center">
+                            <FontAwesomeIcon icon="user-circle" size="3x" />
+                            <h4>Jane Doe</h4>
+                            <h6>15cg00000</h6>
+                            <h6>jane.doe@stu.edu.ng</h6>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Col>
+        );
     }
 }
 
