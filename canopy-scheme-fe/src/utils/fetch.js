f05b08770b2107fc8b88358d9f60a462c952a230
John Ayeni <johnayeni10.aj@gmail.com>
2019-05-29 18:46:05 +0100

    feat: add example env file

--- canopy-scheme-fe/src/utils/fetch.js
+++ canopy-scheme-fe/src/utils/fetch.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3333/api/";
+const API_URL = process.env.REACT_APP_API_URL;
 function getAuthToken() {
     return localStorage.getItem("authToken");
 }
