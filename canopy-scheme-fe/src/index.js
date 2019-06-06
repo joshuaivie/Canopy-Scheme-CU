a9170c0fd14e83a61178c441537227ba07fa054d
John Ayeni <johnayeni10.aj@gmail.com>
2019-06-06 18:23:40 +0100

    Feature/sentry integration frontend (#38)
    * chore: add sentry to frontend
    
    * chore: add sentry to frontend
    
    * fix: remove unnecesary dependencies key in package.json

--- canopy-scheme-fe/src/index.js
+++ canopy-scheme-fe/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+Sentry.init({ dsn: process.env.REACT_APP_SENTRY_KEY });
+// eslint-disable-next-line no-undef
 ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
