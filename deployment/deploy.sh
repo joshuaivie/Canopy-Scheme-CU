7ec6074e9abf360a3f9e766860d37d96032bdb85
Emmanuel Awotunde <awotunde.emmanuel1@gmail.com>
2019-06-20 09:26:39 +0100

    refactor: build images before destorying containers

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,7 +1,9 @@
 export $(grep -v '^#' ../canopy-scheme-be/.env | xargs -d '\n')
 
-docker-compose down
-docker-compose up --build -d
+docker-compose build --no-cache
+docker-compose stop
+docker-compose rm -f
+docker-compose up -d
 
 docker-compose run -d --rm be adonis migration:run --force
 docker-compose run -d be adonis kue:listen
