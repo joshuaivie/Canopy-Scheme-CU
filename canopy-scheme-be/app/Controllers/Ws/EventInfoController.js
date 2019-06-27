c73c5d1cbbd459f9b5fd58f6087c47c04f947dbc
Emmanuel Awotunde <emmanuel.awotunde@stu.cu.edu.ng>
2019-06-27 09:26:55 +0100

    chore: add more matric numbers to files (#90)

---
+++ canopy-scheme-be/app/Controllers/Ws/EventInfoController.js
@@ -0,0 +1,10 @@
+"use strict";
+
+class EventInfoController {
+  constructor({ socket, request }) {
+    this.socket = socket;
+    this.request = request;
+  }
+}
+
+module.exports = EventInfoController;
